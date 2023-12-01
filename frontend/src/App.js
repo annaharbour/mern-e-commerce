af13e8a00b9f54dc79a1e425ad91d75b4fae103a
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-12-01 12:54:04 -0500

    added backend and api for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import {Outlet} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      E-Commerce
-    </div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import './assets/bootstrap.custom.css'
+import './assets/index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+const router = (
+  <Router>
+    <Routes>
+    <Route path='/' element={<App/>}>
+      <Route index={true} path="/" element={<HomeScreen/>}/>
+      <Route path='/product/:id' element={<ProductScreen/>} />
+
+    </Route>
+    </Routes>
+    </Router>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {router}
   </React.StrictMode>
 );
 
