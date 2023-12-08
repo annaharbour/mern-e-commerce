33f69951c3e481f0dd303bfc525c0f6bc72766a6
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-12-08 09:57:07 -0500

    Shipping screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = (
   <Router>
@@ -26,6 +27,7 @@ const router = (
       <Route path='/cart' element={<CartScreen/>}/>
       <Route path='/login' element={<LoginScreen/>}/>
       <Route path='/register' element={<RegisterScreen/>}/>
+      <Route path='shipping' element={<ShippingScreen/>}/>
 
     </Route>
     </Routes>
