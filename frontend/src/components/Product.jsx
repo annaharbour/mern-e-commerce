1b5e3f56d57d5d56d3d66050b8c18a18c2f8cfae
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-12-12 12:04:06 -0500

    Fixed image link in product component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,7 +6,7 @@ import Rating from './Rating';
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/$product._id`}>
+        <a href={`/product/${product._id}`}>
             <Card.Img src={product.image} variant="top"/>
         </a>
         <Card.Body>
